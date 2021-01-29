63bbd973db23be5287a9662fbcba38a8ef371b98
waffelroffel <waffelroffel@gmail.com>
2021-01-29 21:59:28 +0100

    formatting

--- src/Watcher.ts
+++ src/Watcher.ts
@@ -1,41 +1,39 @@
-import Ship from "./Ship";
+import Ship from "./Ship"
 import { FSWatcher, watch as chokidarWatch } from "chokidar"
-import { EventEmitter } from "events";
+import { EventEmitter } from "events"
 
 export default class Watcher extends EventEmitter {
-	private root: string;
-	private watcher: FSWatcher | null;
-	private magellan: Ship;
+	private root: string
+	private watcher: FSWatcher | null
+	private magellan: Ship
 
 	constructor(magellan: Ship, root: string) {
-		super();
-		this.magellan = magellan;
-		this.root = root;
-		this.watcher = null;
+		super()
+		this.magellan = magellan
+		this.root = root
+		this.watcher = null
 	}
 
 	watch(folder?: string): void {
-		folder = folder ?? this.root;
+		folder = folder ?? this.root
 
 		// TODO since the nested file/folder watcher is not used, find other watchers or builtin functions?
 		this.watcher = chokidarWatch(folder, {
 			persistent: true,
-			depth: 0
-		});
+			depth: 0,
+		})
 
 		this.watcher
 			.on("add", (path: string) => {
-				this.magellan.emit("add", path);
+				this.magellan.emit("add", path)
 			})
 			.on("change", (path: string) => {
-				this.magellan.emit("change", path);
+				this.magellan.emit("change", path)
 			})
 			.on("unlink", (path: string) => {
-				this.magellan.emit("remove", path);
+				this.magellan.emit("remove", path)
 			})
-			.on("error", (error: any) =>
-				this.magellan.emit("error", error)
-			)
-			.on("ready", () => this.magellan.emit("ready"));
+			.on("error", (error: any) => this.magellan.emit("error", error))
+			.on("ready", () => this.magellan.emit("ready"))
 	}
 }
